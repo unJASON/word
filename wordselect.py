a1e8ed5e50fb7b6532348767a012000c78e369d1
unJASON <2693017973@qq.com>
2018-10-15 12:19:53 +0800

    。。
    。。

--- wordselect.py
+++ wordselect.py
@@ -9,10 +9,15 @@ import codecs
 jieba.load_userdict("userdic.txt")  # 用户词典，自带词典不一定符合语境
 default_encoding = "utf-8"
 try:
-    f = open('E:/myfile/resource.csv', 'rb')  # 源csv文本
+    f = open('data/resource.csv', 'rb')  # 源csv文本
     line = f.readline()
     count = 0
     ct = 0
+
+    f1 = open('stopwords.txt', 'rb')
+    line1 = f1.readline()
+    stopwords = line1.decode('utf-8').split(' ')
+    f1.close()
     """以天为单位存储处理的数据"""
     list1 = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
              '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
@@ -20,9 +25,7 @@ try:
     while line:
         desc = line.decode('GBK', 'ignore').split(',')[5]
         fenci_text = jieba.cut(desc)
-        f1 = open('stopwords.txt', 'rb')
-        line1 = f1.readline()
-        stopwords = line1.decode('gb2312').split(' ')
+
         final = ""
         for word in fenci_text:
             if word not in stopwords:
@@ -39,12 +42,14 @@ try:
         print " "
         """
         # 时间格式2016/3/8  14:35:19
-        time = line.split(',')[15]
-        sj = time[8:9]
+        # print(line.decode('GBK', 'ignore'))
+        #数据图片都有逗号,应当取最后一个数组
+        time = line.decode('GBK', 'ignore').split(',')[-1]
+        sj = time[8:10]
         sj = ''.join(sj.split())
         while ct < 31:
             if list1[ct] == sj:
-                a = '/shuju/'+sj+'.txt'
+                a = 'shuju/'+sj+'.txt'
                 f2 = codecs.open(a, 'a', 'utf-8')
                 try:
                     f2.write(json.dumps(segs,ensure_ascii=False))
@@ -62,6 +67,7 @@ try:
         if count % 10000 == 0:
             print("current lines:", count)
         line = f.readline()
+    f.close()
 
 except IOError as err:
     print('File error:', str(err))
